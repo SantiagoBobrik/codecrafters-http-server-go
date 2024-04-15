d30317d38903c568084b1aa71ebfc72aa3187267
Santiago Diaz <santiago.bobrik@gmail.com>
2024-04-15 00:26:06 -0300

    add not found handler

--- app/server.go
+++ app/server.go
@@ -2,34 +2,65 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strings"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+type Request struct {
+	method   string
+	path     string
+	protocol string
+}
 
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+var request = &Request{}
+
+var successResponse = "HTTP/1.1 200 OK\r\n\r\n"
+var notFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Failed to accept connection")
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+
+	fmt.Println("Accepted connection")
+
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+
+	_, err := conn.Read(buf)
 
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-
+		log.Fatal("Failed to read data")
 	}
 
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	if err != nil {
-		fmt.Println("Failed to write response: ", err.Error())
-		os.Exit(1)
+	reqStringSlice := strings.Split(string(buf), "\r\n")
+	startLineSlice := strings.Split(reqStringSlice[0], " ")
+
+	request.method = startLineSlice[0]
+	request.path = startLineSlice[1]
+	request.protocol = startLineSlice[2]
+
+	if request.path != "/" {
+		conn.Write([]byte(notFoundResponse))
+		return
 	}
 
+	conn.Write([]byte(successResponse))
 }
