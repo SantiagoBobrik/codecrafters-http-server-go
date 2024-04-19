f290598255625980c5ec89fc755bb35b71cd0df8
Santiago Diaz <santiago.bobrik@gmail.com>
2024-04-18 23:22:30 -0300

    add notfound

--- app/server.go
+++ app/server.go
@@ -14,10 +14,42 @@ type Request struct {
 	protocol string
 }
 
-var request = &Request{}
 var successResponse = "HTTP/1.1 200 OK\r\n\r\n"
 var notFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n"
 var contentType = "Content-Type: text/plain \r\n\r\n"
+
+func newRequest(method string, path string, protocol string) *Request {
+	return &Request{method, path, protocol}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+
+	if err != nil {
+		log.Fatal("Failed to read data")
+	}
+
+	reqStringSlice := strings.Split(string(buf), "\r\n")
+	startLineSlice := strings.Split(reqStringSlice[0], " ")
+	request := newRequest(startLineSlice[0], startLineSlice[1], startLineSlice[2])
+	ok := false
+
+	fmt.Printf("Request: %s %s %s\n", request.method, request.path, request.protocol)
+
+	handleRoute("/echo", request, func(r *Request) {
+		ok = true
+		paths := strings.Split(request.path, "/")
+		lastPath := paths[len(paths)-1]
+		conn.Write([]byte(successResponse + contentType + lastPath))
+	})
+
+	if !ok {
+		conn.Write([]byte(notFoundResponse))
+	}
+
+}
 
 func main() {
 	listener, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -36,35 +68,11 @@ func main() {
 	}
 }
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
+func handleRoute(path string, request *Request, handler func(r *Request)) {
+	hasPrefix := strings.HasPrefix(request.path, path)
 
-	buf := make([]byte, 1024)
-
-	_, err := conn.Read(buf)
-
-	if err != nil {
-		log.Fatal("Failed to read data")
+	if hasPrefix {
+		handler(request)
 	}
-
-	reqStringSlice := strings.Split(string(buf), "\r\n")
-	startLineSlice := strings.Split(reqStringSlice[0], " ")
-
-	request.method = startLineSlice[0]
-	request.path = startLineSlice[1]
-	request.protocol = startLineSlice[2]
-
-	params := strings.Split(request.path, "/")
-	content := params[len(params)-1] + "\r\n\r\n"
-
-	if params[1] != "echo" {
-		conn.Write([]byte(notFoundResponse))
-		fmt.Printf("404 Not Found: %s\n", request.path)
-		return
-	}
-
-	fmt.Printf("Request: %s %s %s\n", request.method, request.path, request.protocol)
-
-	conn.Write([]byte(successResponse + contentType + content))
 
 }
